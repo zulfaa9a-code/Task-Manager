38b7324b5baad10c9b91fd772dad8ceb36a3e525
zulfaa9a <zulfaa9a@gmail.com>
2026-06-16 13:00:52 +0530

    App.js updated

--- src/app.js
+++ src/app.js
@@ -50,13 +50,13 @@ app.get("/alltask" , async(req, res)=>{
 
 app.use(
   express.static(
-    path.join(__dirname, "../Frontend/dist")
+    path.join(__dirname, "../dist")
   )
 );
 
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../Frontend/dist/index.html")
+    path.join(__dirname, "../dist/index.html")
   );
 });
 module.exports=app;
